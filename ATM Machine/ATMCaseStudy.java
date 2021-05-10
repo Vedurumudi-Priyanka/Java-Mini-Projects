7d65515a23957ddf957ef09f9002275eaf757186
Vedurumudi Priyanka <priyankavedurumudi@gmail.com>
2021-05-10 21:30:00 +0530

    Create ATMCaseStudy.java

---
+++ ATM Machine/ATMCaseStudy.java
@@ -0,0 +1,11 @@
+public class ATMCaseStudy
+{
+   // main method creates and runs the ATM
+   public static void main(String[] args)
+   {
+	  
+      ATM theATM = ATM.getinstance();
+      
+      theATM.run();
+   } // end main
+} // end class ATMCaseStudy 
